c6a51471b77c0104f87150053659e76c5fb8fe83
Cellie <CelliesProjects@users.noreply.github.com>
2020-12-05 14:32:22 +0100

    Use a single websocket. (#2)

--- smartMeterLogger-esp32.ino
+++ smartMeterLogger-esp32.ino
@@ -16,6 +16,7 @@ const char*    WS_BRIDGE_URL =     "/raw";                   /* bridge url */
 
 #include "wifisetup.h"
 #include "index_htm.h"
+#include "vandaag_htm.h"
 
 #if defined(SH1106_OLED)
 #include <SH1106.h>                /* Install via 'Manage Libraries' in Arduino IDE -> https://github.com/ThingPulse/esp8266-oled-ssd1306 */
@@ -47,13 +48,11 @@ const IPAddress SUBNET(255, 255, 255, 0);                /* Usually 255,255,255,
 const IPAddress PRIMARY_DNS(192, 168, 0, 30);            /* Check in your router */
 const IPAddress SECONDARY_DNS(192, 168, 0, 50);          /* Check in your router */
 
-const char*     WS_RAW_URL = "/raw";
-const char*     WS_CURRENT_URL = "/current";
+const char*     WS_EVENTS_URL = "/events";
 
 WebSocketsClient ws_bridge;
 AsyncWebServer  server(80);
-AsyncWebSocket  ws_raw(WS_RAW_URL);
-AsyncWebSocket  ws_current(WS_CURRENT_URL);
+AsyncWebSocket  ws_events(WS_EVENTS_URL);
 HardwareSerial  smartMeter(UART_NR);
 
 #if defined(SH1106_OLED)
@@ -131,11 +130,8 @@ void setup() {
   time(&bootTime);
 
   /* websocket setup */
-  ws_raw.onEvent(ws_server_onEvent);
-  server.addHandler(&ws_raw);
-
-  ws_current.onEvent(ws_server_onEvent);
-  server.addHandler(&ws_current);
+  ws_events.onEvent(ws_server_onEvent);
+  server.addHandler(&ws_events);
 
   /* webserver setup */
   static char modifiedDate[30];
@@ -147,6 +143,13 @@ void setup() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (htmlUnmodified(request, modifiedDate)) return request->send(304);
     AsyncWebServerResponse *response = request->beginResponse_P(200, HTML_MIMETYPE, index_htm, index_htm_len);
+    response->addHeader(HEADER_LASTMODIFIED, modifiedDate);
+    request->send(response);
+  });
+
+  server.on("/vandaag", HTTP_GET, [](AsyncWebServerRequest * request) {
+    if (htmlUnmodified(request, modifiedDate)) return request->send(304);
+    AsyncWebServerResponse *response = request->beginResponse_P(200, HTML_MIMETYPE, vandaag_htm, vandaag_htm_len);
     response->addHeader(HEADER_LASTMODIFIED, modifiedDate);
     request->send(response);
   });
@@ -177,6 +180,12 @@ static uint32_t numberOfSamples{0};
 void saveAverage(const tm& timeinfo) {
   String path{'/' + String(timeinfo.tm_year + 1900)}; /* add the current year to the path */
 
+  const String message {
+    String(time(NULL)) + " " + String(average / numberOfSamples)
+  };
+
+  ws_events.textAll("electric_saved\n" + message);
+
   File folder = SD.open(path);
   if (!folder && !SD.mkdir(path))
     ESP_LOGE(TAG, "could not create folder %s", path);
@@ -200,10 +209,6 @@ void saveAverage(const tm& timeinfo) {
     booted = false;
   }
 
-  const String message {
-    String(time(NULL)) + " " + String(average / numberOfSamples)
-  };
-
   ESP_LOGI(TAG, "%i samples - saving '%s' to file '%s'", numberOfSamples, message.c_str(), path.c_str());
 
   appendToFile(path.c_str(), message.c_str());
@@ -213,8 +218,7 @@ void saveAverage(const tm& timeinfo) {
 }
 
 void loop() {
-  ws_raw.cleanupClients();
-  ws_current.cleanupClients();
+  ws_events.cleanupClients();
 
   /* save the average power consumption to SD every 'SAVE_TIME_MIN' minutes */
   static struct tm now;
@@ -251,7 +255,7 @@ void ws_server_onEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, Aws
 
     case WS_EVT_CONNECT :
       ESP_LOGD(TAG, "[%s][%u] connect", server->url(), client->id());
-      if (0 == strcmp(WS_CURRENT_URL, server->url()))
+      if (0 == strcmp(WS_EVENTS_URL, server->url()))
         client->text(currentUseString);
       break;
 
@@ -331,9 +335,6 @@ bool appendToFile(const char* path, const char* message) {
 }
 
 void process(const String& telegram) {
-
-  ws_raw.textAll(telegram);
-
   using decodedFields = ParsedData <
                         /* FixedValue */ energy_delivered_tariff1,
                         /* FixedValue */ energy_delivered_tariff2,
@@ -376,7 +377,7 @@ void process(const String& telegram) {
   average += data.power_delivered.int_val();
   numberOfSamples++;
 
-  snprintf(currentUseString, sizeof(currentUseString), "%i\n%i\n%i\n%i\n%i\n%i\n%i\n%s",
+  snprintf(currentUseString, sizeof(currentUseString), "current\n%i\n%i\n%i\n%i\n%i\n%i\n%i\n%s",
            data.power_delivered.int_val(),
            data.energy_delivered_tariff1.int_val(),
            data.energy_delivered_tariff2.int_val(),
@@ -387,7 +388,7 @@ void process(const String& telegram) {
            (data.electricity_tariff.equals("0001")) ? "laag" : "hoog"
           );
 
-  ws_current.textAll(currentUseString);
+  ws_events.textAll(currentUseString);
 
   if (oledFound) {
     oled.clear();
