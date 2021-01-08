cf2454b553866ed798099892fb93486fa213a086
Cellie <CelliesProjects@users.noreply.github.com>
2021-01-08 16:43:54 +0100

    Moved static ip setup to wifisetup.h

--- smartMeterLogger-esp32.ino
+++ smartMeterLogger-esp32.ino
@@ -40,14 +40,6 @@ const char*    WS_BRIDGE_URL =     "/raw";                   /* bridge url */
 const char* NTP_POOL =                  "nl.pool.ntp.org";
 const char* TIMEZONE =                  "CET-1CEST,M3.5.0/2,M10.5.0/3"; /* Central European Time - see http://www.remotemonitoringsystems.ca/time-zone-abbreviations.php */
 
-#define SET_STATIC_IP                   false            /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
-
-const IPAddress STATIC_IP(192, 168, 0, 90);              /* This should be outside your router dhcp range! */
-const IPAddress GATEWAY(192, 168, 0, 1);                 /* Set to your gateway ip address */
-const IPAddress SUBNET(255, 255, 255, 0);                /* Usually 255,255,255,0 check in your router or pc connected to the same network */
-const IPAddress PRIMARY_DNS(192, 168, 0, 30);            /* Check in your router */
-const IPAddress SECONDARY_DNS(192, 168, 0, 50);          /* Check in your router */
-
 const char*     WS_RAW_URL = "/raw";
 const char*     WS_EVENTS_URL = "/events";
 
--- wifisetup.h
+++ wifisetup.h
@@ -1,2 +1,10 @@
 const char* WIFI_NETWORK = "xxx";
 const char* WIFI_PASSWORD = "xxx";
+
+#define SET_STATIC_IP false                              /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
+
+const IPAddress STATIC_IP(192, 168, 0, 90);              /* This should be outside your router dhcp range! */
+const IPAddress GATEWAY(192, 168, 0, 1);                 /* Set to your gateway ip address */
+const IPAddress SUBNET(255, 255, 255, 0);                /* Usually 255,255,255,0 check in your router or pc connected to the same network */
+const IPAddress PRIMARY_DNS(192, 168, 0, 30);            /* Check in your router */
+const IPAddress SECONDARY_DNS(192, 168, 0, 50);          /* Check in your router */
